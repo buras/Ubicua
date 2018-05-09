dd58b64b1bde56353175688958935ff1f296a547
buras <burillocbc@gmail.com>
2018-05-09 15:27:44 +0200

    proyecto añadido

---
+++ Ubicua/src/ubicua/Ubicua.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ubicua;
+
+/**
+ *
+ * @author Don Portatil
+ */
+public class Ubicua {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        VistaGlobal x = new VistaGlobal();
+        x.setVisible(true);
+    }
+    
+}
